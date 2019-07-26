6ba52b28a034bc9ed529ca11a289843e62a24735
gaotong6434 <49805607+gaotong6434@users.noreply.github.com>
2019-07-26 21:32:28 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("高通大傻子");
+        System.out.println("高通好帅");
     }
 
 }
